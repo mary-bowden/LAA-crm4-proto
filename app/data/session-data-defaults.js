717954631bf4852708bf5c53a4312f7e874f9a79
Mary Bowden <mazzabodwin@googlemail.com>
2023-08-24 17:35:11 +0100

    First push

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -2,4 +2,22 @@ module.exports = {
 
   // Insert values here
 
+  //Key default values to set up proto
+  "who-signin": "provider",
+
+  //Key users
+
+  "provider1": "Jonanna Barton",
+  "provider1-permiss": "Admin",
+
+  "provider2": "Simi Sunlight",
+  "provider2-permiss": "User",
+
+
+  "caseworker1": "Tattina Wolf",
+  "caseworker1-permiss": "Admin",
+
+  "caseworker2": "Johanna Justice",
+  "caseworker2-permiss": "User",
+
 }
